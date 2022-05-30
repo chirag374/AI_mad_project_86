ea4a9479775b24872560c9562647aad658c1093e
ChiragBhargava <chiragbhargava91@gmail.com>
2022-05-31 01:03:21 +0530

    buttons and interpretation added

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -26,41 +26,44 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
-    int matchCounter=0;
-    int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
-    int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
-    String operators[]={"+","-","*","/"};
-    int correctButton=0; //which button will have the correct answer (tag of that button)
-    Random random=new Random(); //You will generate randdom alegebra questions
+    int matchCounter = 0;
+    int[] performance = {-1, -1, -1, -1, -1, -1}; //score of a game is updated in this array
+    int[] score = {-1, -1, -1}; //score of each match is updated in this array. A total of three matches in a game
+    String operators[] = {"+", "-", "*", "/"};
+    Random random = new Random(); //You will generate random algebra questions
+    int correctButton = 0; //which button will have the correct answer (tag of that button)
+
     TextView textView2;
-    Button button1,button2,button3,button4;
-    public void load(View view){
-        Button buttonClicked=(Button)view;
-        if(buttonClicked.getTag().toString().equals(correctButton+"")){
-            score[matchCounter++]=1;
-        }else{
-            score[matchCounter++]=0;
+    Button button1, button2, button3, button4;
+
+    public void load(View view) {
+        Button buttonClicked = (Button) view;
+        if (buttonClicked.getTag().toString().equals(correctButton + "")) {
+            score[matchCounter++] = 1;
+        } else {
+            score[matchCounter++] = 0;
         }
         newMatch();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1=findViewById(R.id.button1);
-        button2=findViewById(R.id.button2);
-        button3=findViewById(R.id.button3);
-        button4=findViewById(R.id.button4);
-        textView2=findViewById(R.id.textView2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        textView2 = findViewById(R.id.textView2);
         newMatch();
-        sharedPreferences=this.getSharedPreferences("com.example.aiapp_2022", Context.MODE_PRIVATE);
-        int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimenssional array
-        double slope=LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
+        sharedPreferences = this.getSharedPreferences("com.example.ai_app_2022", Context.MODE_PRIVATE);
+        int[][] dataFrame = dataPrep(); //dataPrep function returns a two-dimensional array
+        double slope = LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
         new AlertDialog.Builder(this)
-               // .setIcon() //your custom icon
+                // .setIcon() //your custom icon
                 .setTitle("Performance")
 
-                .setMessage(getInterpretation(dataFrame,slope))
+                .setMessage(getInterpretation(dataFrame, slope))
                 .setNeutralButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -72,30 +75,65 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2 = random.nextInt(10);
+        int correctAns = -100;
+        correctButton = random.nextInt(4);
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+        // Your code here, to diplay correct and incorrect options on the button
+        if (operator.equals("+")) {
+            correctAns = operand1 + operand2;
+        } else if (operator.equals("-")) {
+            correctAns = operand1 - operand2;
+        } else if (operator.equals("/")) {
+            correctAns = operand1 / operand2;
+        } else if (operator.equals("*")) {
+            correctAns = operand1 * operand2;
+        }
+        if (correctButton == 0) {
+            button1.setText(correctAns + "");
+            button2.setText(correctAns + 1 + "");
+            button3.setText(correctAns - 1 + "");
+            button4.setText(correctAns + 2 + "");
+        } else if (correctButton == 1) {
+            button1.setText(correctAns + 1 + "");
+            button2.setText(correctAns + "");
+            button3.setText(correctAns - 1 + "");
+            button4.setText(correctAns + 2 + "");
+        } else if (correctButton == 2) {
+            button1.setText(correctAns + 1 + "");
+            button2.setText(correctAns - 1 + "");
+            button3.setText(correctAns + "");
+            button4.setText(correctAns + 2 + "");
+        } else if (correctButton == 3) {
+            button1.setText(correctAns + 1 + "");
+            button2.setText(correctAns + 2 + "");
+            button3.setText(correctAns - 1 + "");
+            button4.setText(correctAns + "");
+        }
 
-        if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
+        if (matchCounter == 3) {    // if three matches are completed update the performance in shared preferences
 
-            matchCounter=0;
+            matchCounter = 0;
 
-            for(int i=0;i<performance.length-1;i++){ //adjusting the performance array so that last six entries present with the most recent at the last index.
-                performance[i]=performance[i+1];
+            for (int i = 0; i < performance.length - 1; i++) { //adjusting the performance array so that last six entries present with the most recent at the last index.
+                performance[i] = performance[i + 1];
             }
-            performance[5]=sumOfScore(); //calculating the sum of last three matches (note result of a match is 1 ro 0, and add to performance
-            sharedPreferences.edit().putString("data",new Gson().toJson(performance)).apply();
+            performance[5] = sumOfScore(); //calculating the sum of last three matches (note result of a match is 1 ro 0, and add to performance
+            sharedPreferences.edit().putString("data", new Gson().toJson(performance)).apply();
 
         }
     }
 
-    public int sumOfScore(){
+    public int sumOfScore() {
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
-        int sum=0;
-       // your code here
+        int sum = 0;
+        // your code here
+        for (int i = 0; i < score.length; i++) {
+            sum = sum + score[i];
+        }
         return sum;
     }
 
@@ -103,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         int[] data = new Gson().fromJson((sharedPreferences.getString("data", null)), performance.getClass());
         Log.i("data", Arrays.toString(data)); //this is how you display arrays in Logcat, for debugging
         int dataFrame[][] = new int[6][2]; //creating a dataframe of two columns and six rows for regresson purpose
-        if(data==null)
+        if (data == null)
             return null;
         for (int i = 0; i < data.length; i++) {
             dataFrame[i][0] = i + 1;
@@ -112,9 +150,23 @@ public class MainActivity extends AppCompatActivity {
         return dataFrame;
     }
 
-    public String getInterpretation(int [][]dataFrame,double slope){
-       //provide interpretation based on your slope analysis
+    public String getInterpretation(int[][] dataFrame, double slope) {
+        String Interpretation = "all is well";
+        if (slope > 0 && slope <= .5)
+            Interpretation = "you are a slow learner";
+        else if (slope > .5)
+            Interpretation = "you are a good learner";
+        else if (slope < 0)
+            Interpretation = "you are an unlearner";
+        else if (dataFrame[0][1] == 3 && slope == 0)
+            Interpretation = "you achieved perfection";
+        else if (dataFrame[0][1] == 0 && slope == 0)
+            Interpretation = "you do not learned";
+
+
+        //provide interpretation based on your slope analysis
         // Your code here
-        return "Your Interpretation";
+
+        return Interpretation;
     }
 }
